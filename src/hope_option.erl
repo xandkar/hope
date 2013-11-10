e492ea132d3b73d2597fc8b832128dc40fed63ba
Siraaj Khandkar <siraaj@khandkar.net>
2013-11-10 11:32:28 -0500

    Add option type module.

---
+++ src/hope_option.erl
@@ -0,0 +1,27 @@
+-module(hope_option).
+
+-export_type([ t/1
+             ]).
+
+-export([ put/2
+        , get/2
+        , map/2
+        ]).
+
+
+-type t(A) :: none | {some, A}.
+
+
+put(X, F) ->
+    case F(X)
+    of  true  -> {some, X}
+    ;   false -> none
+    end.
+
+
+get({some, X}, _) -> X;
+get(none     , Y) -> Y.
+
+
+map({some, X}, F) -> {some, F(X)};
+map(none     , _) -> none.
